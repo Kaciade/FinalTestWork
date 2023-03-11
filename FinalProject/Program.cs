0abd9f71e42a6482a17802bcd6bdd78f0eaa684f
Kaciade <valerypolyakov60@gmail.com>
2023-03-11 20:01:25 +0400

    Программа полностью изменена и готова

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,22 +1,33 @@
-﻿string[] array1 = {"Hungary", "Russia", "Germany", "United Kingdom", 
-                "France", "Denmark", "Spain", "Romania", "Italy", 
-                "Netherlands", "Belgium", "Czech Republic (Czechia)", 
-                "Greece", "Portugal", "Sweden"};
-string[] array2 = new string [array1.Length];
+﻿Console.Clear();
+
+string[] array1 = {"123", "15", "Germany", "XD", 
+                "France", "Denmark", "LOL", "adpgkn", "xyz", 
+                "Netherlands", "AZN", "RUS", 
+                "Greece", "0987q4t", ":-)"};
 
 Console.WriteLine("\nПервоначальный массив:");
-for (int i = 0; i < array1.Length; i++) Console.Write($"{array1[i]}\t");
-
-for (int i = 0; i < array1.Length; i++) array2[i] = array1[i];
+for (int i = 0; i < array1.Length; i++) Console.Write($"{array1[i]}   |   ");
 
 int count = 0;
+Console.WriteLine();
 for (int i = 0; i < array1.Length; i++)
 {
-    if (count == 2)
+    if (array1[i].Length <= 3)
     {
-        Console.WriteLine();
-        count = 0;
+        count++;
     }
-    Console.Write($"{array1[i]}\t");
-    count++;
 }
+
+int j = 0;
+string[] array2 = new string [count];
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <= 3)
+    {
+        array2[j] = array1[i];
+        j++;
+    }
+}
+
+Console.WriteLine("\nНовый массив из строк, длина которых меньше либо равна 3 символам:");
+for (int i = 0; i < array2.Length; i++) Console.Write($"{array2[i]}   |   ");
