ba2846b56a23b72c8e415ad71108c96d7393ea2e
Kaciade <valerypolyakov60@gmail.com>
2023-03-11 19:29:34 +0400

    Программа почти завершена

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,14 +1,22 @@
-﻿Console.Write("\nУкажите количество элементов массива: ");
-int length = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите левую часть диапазона массива: ");
-int min = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите правую часть диапазона массива: ");
-int max = int.Parse(Console.ReadLine() ?? "0");
-int[] array = new int [length];
-Random random = new Random();
-
-for (int i = 0; i < array.Length; i++) array[i] = random.Next(min, max);
+﻿string[] array1 = {"Hungary", "Russia", "Germany", "United Kingdom", 
+                "France", "Denmark", "Spain", "Romania", "Italy", 
+                "Netherlands", "Belgium", "Czech Republic (Czechia)", 
+                "Greece", "Portugal", "Sweden"};
+string[] array2 = new string [array1.Length];
 
 Console.WriteLine("\nПервоначальный массив:");
-for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}\t");
+for (int i = 0; i < array1.Length; i++) Console.Write($"{array1[i]}\t");
 
+for (int i = 0; i < array1.Length; i++) array2[i] = array1[i];
+
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (count == 2)
+    {
+        Console.WriteLine();
+        count = 0;
+    }
+    Console.Write($"{array1[i]}\t");
+    count++;
+}
